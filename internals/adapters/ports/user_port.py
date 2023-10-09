4542566659381021ec0e76864d424ff57204b7d7
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-10-09 16:49:52 +0700

    :money_with_wings: config cors

--- internals/adapters/ports/user_port.py
+++ internals/adapters/ports/user_port.py
@@ -1,19 +1,26 @@
 from flask import Blueprint, request
+from flask_cors import cross_origin
 from internals.app.handlers.user_handler import UserHandler
 
 user_port_bp = Blueprint("user_port", __name__)
 
+
 @user_port_bp.route("/users/list", methods=["GET"])
+@cross_origin()
 def get_all_users():
     users = UserHandler.get_all_users()
     return users
 
+
 @user_port_bp.route("/users/find/<int:user_id>", methods=["GET"])
+@cross_origin()
 def get_user_by_id(user_id):
     user = UserHandler.get_user_by_id(user_id)
     return user
 
+
 @user_port_bp.route("/users/find", methods=["GET"])
+@cross_origin()
 def get_user_by_email_or_username():
     email_or_username = request.args.get("email_or_username")
     user = UserHandler.get_user_by_email_or_username(email_or_username)
@@ -21,16 +28,21 @@ def get_user_by_email_or_username():
 
 
 @user_port_bp.route("/users/create", methods=["POST"])
+@cross_origin()
 def create_user():
     data = request.get_json()
     user = UserHandler.create_user(data)
     return user
 
+
 @user_port_bp.route("/users/update/<int:user_id>", methods=["PUT"])
+@cross_origin()
 def update_user(user_id):
     data = request.get_json()
     return UserHandler.update_user(user_id, data)
 
+
 @user_port_bp.route("/users/delete/<int:user_id>", methods=["DELETE"])
+@cross_origin()
 def delete_user(user_id):
     return UserHandler.delete_user(user_id)
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from internals.adapters.ports.main_port import init_port
 from pkg.configs.database import db, init_db, migrate_db
 from pkg.configs.config import config_app
 
+
 def create_app():
     app = Flask(__name__)
 
+    CORS(app, resources={r"/apis/*": {"origins": "*"}})
+
     environment = os.environ.get("FLASK_ENV")
     app.config.from_object(config_app[environment])
+    app.config["CORS_HEADERS"] = "Content-Type"
 
     init_db(app)
     migrate_db(app, db)
@@ -16,6 +21,7 @@ def create_app():
     init_port(app)
 
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
